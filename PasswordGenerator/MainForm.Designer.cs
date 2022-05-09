b16a59ae7b44bd992f91397ede9fcf3bf542f593
xrusteam <90495504+xrusteam@users.noreply.github.com>
2022-05-09 16:21:24 +0300

    Добавьте файлы проекта.

---
+++ PasswordGenerator/MainForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace PasswordGenerator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.logBtn = new System.Windows.Forms.Button();
+            this.changeBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelLogin.Location = new System.Drawing.Point(99, 83);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(90, 36);
+            this.labelLogin.TabIndex = 0;
+            this.labelLogin.Text = "Login";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPassword.Location = new System.Drawing.Point(99, 151);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(147, 36);
+            this.labelPassword.TabIndex = 1;
+            this.labelPassword.Text = "Password";
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Location = new System.Drawing.Point(353, 89);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(179, 22);
+            this.loginTextBox.TabIndex = 2;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(353, 155);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(179, 22);
+            this.passwordTextBox.TabIndex = 3;
+            // 
+            // logBtn
+            // 
+            this.logBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.logBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.logBtn.Location = new System.Drawing.Point(105, 251);
+            this.logBtn.Name = "logBtn";
+            this.logBtn.Size = new System.Drawing.Size(141, 48);
+            this.logBtn.TabIndex = 4;
+            this.logBtn.Text = "Войти";
+            this.logBtn.UseVisualStyleBackColor = true;
+            // 
+            // changeBtn
+            // 
+            this.changeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.changeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.changeBtn.Location = new System.Drawing.Point(353, 251);
+            this.changeBtn.Name = "changeBtn";
+            this.changeBtn.Size = new System.Drawing.Size(189, 48);
+            this.changeBtn.TabIndex = 5;
+            this.changeBtn.Text = "Изменить пароль";
+            this.changeBtn.UseVisualStyleBackColor = true;
+            this.changeBtn.Click += new System.EventHandler(this.changeBtn_Click);
+            // 
+            // PasswordGeneratorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 351);
+            this.Controls.Add(this.changeBtn);
+            this.Controls.Add(this.logBtn);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.loginTextBox);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelLogin);
+            this.Name = "PasswordGeneratorForm";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button logBtn;
+        private System.Windows.Forms.Button changeBtn;
+    }
+}
+
---
+++ PasswordGenerator/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordGenerator
+{
+    public partial class MainForm : Form
+    {
+        PasswordGeneratorForm form = null;
+        void f_Disposed(object sender, EventArgs e)
+        {
+            form = null;
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.BackColor = Color.FromArgb(224, 191, 230);
+            this.labelLogin.ForeColor = Color.FromArgb(102, 45, 145);
+            this.labelPassword.ForeColor = Color.FromArgb(102, 45, 145);
+            this.logBtn.BackColor = Color.FromArgb(102, 45, 145);
+            this.logBtn.ForeColor = Color.FromArgb(224, 191, 230);
+            this.changeBtn.BackColor = Color.FromArgb(102, 45, 145);
+            this.changeBtn.ForeColor = Color.FromArgb(224, 191, 230);
+        }
+        
+
+        private void changeBtn_Click(object sender, EventArgs e)
+        {
+           if(form == null)
+           {
+                form = new PasswordGeneratorForm();
+                form.Disposed += new EventHandler(f_Disposed);
+                form.Show();
+           } 
+        }
+        
+    }
+}
